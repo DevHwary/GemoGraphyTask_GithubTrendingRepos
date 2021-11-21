1a5a1ef1958b3bc950895028810d29669a8e1886
Mohamed Elhwary <mh.hwary@yahoo.com>
2021-11-22 01:45:25 +0200

    add list_languages

--- trending_repos/github/utils.py
+++ trending_repos/github/utils.py
@@ -3,13 +3,52 @@ import requests
 
 
 def fetch_github_url_repos(days:int, nom_repos:int):
-    
     try:
         days_ago = (datetime.now() - timedelta(days=days)).strftime("%Y-%m-%d")
         url = "https://api.github.com/search/repositories?q=created:>{0}&sort=stars&order=desc&page=1&per_page={1}".format(days_ago, nom_repos)
         repos = requests.get(url)
         return repos
-
-
     except requests.exceptions.HTTPError as e:
         return (e.response.text)
+
+
+def list_languages(repos):
+    """
+    Input : repos, respone of fetch_github_url_repos(). 
+    Output: returns Dict of objects, one object structure: 
+        "Python" : {
+            "Number of repos": 17:int,
+            "List of repos": [
+                [
+                    "repo_URL",
+                    "HTML_URL"
+                ],
+                [
+                    "repo_URL",
+                    "HTML_URL"
+                ]
+            ]
+        }
+    """
+    try:
+        repos_dict = repos.json()['items']
+        languages = {}
+
+        NO_OF_REPOS = "Number of repos"
+        LIST_OF_REPOS = "List of repos"
+        URL = "url"
+        HTML_URL = "html_url"
+
+        for repo in repos_dict:
+            language = repo['language']
+            # From inside the languages DICT. Get or create the current repository's language data.
+            prevEntry = languages.setdefault(language,
+                                            {NO_OF_REPOS: 0,
+                                            LIST_OF_REPOS: []})
+
+            languages[language][NO_OF_REPOS] = prevEntry[NO_OF_REPOS] + 1
+            languages[language][LIST_OF_REPOS].append({repo[URL], repo[HTML_URL]})
+        return languages
+
+    except Exception as e:
+        return ({"error message": "{0}".format(str(e))})
--- trending_repos/github/views.py
+++ trending_repos/github/views.py
@@ -1,15 +1,16 @@
-from django.http import response
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
 from rest_framework import status
-from .utils import fetch_github_url_repos
+from .utils import fetch_github_url_repos, list_languages
 from rest_framework.parsers import JSONParser
 
 
 @parser_classes([JSONParser])
 @api_view(['GET'])
 def get_github_trending_repos(request):
-    if request.method == "GET":
+    try:
         github_response = fetch_github_url_repos(days=30, nom_repos=100)
-        return Response( github_response.json()['items'], status=status.HTTP_200_OK)
-    
+        languages = list_languages(github_response)
+        return Response( languages, status=status.HTTP_200_OK)
+    except BaseException as e:
+        return Response({"error message": "{0}".format(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
