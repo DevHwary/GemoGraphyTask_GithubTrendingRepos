990d9e82102c8bf5e9884c6d81732d685c9dc269
Mohamed Elhwary <mh.hwary@yahoo.com>
2021-11-22 02:17:51 +0200

    add tests for API views

--- trending_repos/github/tests.py
+++ trending_repos/github/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import datetime
+from rest_framework import status
+from rest_framework.test import APITestCase
+import json
 
-# Create your tests here.
+
+class GithubTests(APITestCase):
+    """
+    API View tests
+    """
+    def test_fetch_github_url_repos(self):
+        url = '/github/trending_repos/'
+        response = self.client.get(url)
+        result = json.loads(response.content)   
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(type(result), dict )
+        self.assertEqual(type(result['Python']["Number of repos"]), int )
