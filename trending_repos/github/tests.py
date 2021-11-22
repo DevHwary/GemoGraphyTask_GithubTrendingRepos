fae99adfeff823063c7a67de3bef1c4dec10f59d
Mohamed Elhwary <mh.hwary@yahoo.com>
2021-11-22 02:50:12 +0200

    add tests, test_list_languages

--- trending_repos/github/tests.py
+++ trending_repos/github/tests.py
@@ -1,9 +1,8 @@
-import datetime
 import json
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .utils import fetch_github_url_repos
+from .utils import (fetch_github_url_repos, list_languages)
 
 
 class GithubTests(APITestCase):
@@ -25,5 +24,10 @@ class UtilsTests(TestCase):
         github_response = fetch_github_url_repos(days=30, nom_repos=100)
         self.assertEqual(github_response.status_code, status.HTTP_200_OK)
 
+
+    def test_list_languages(self):
+        github_response = fetch_github_url_repos(days=30, nom_repos=100)
+        languages = list_languages(github_response)
+        self.assertEqual(type(languages["Python"]["Number of repos"]), int )
 
 
