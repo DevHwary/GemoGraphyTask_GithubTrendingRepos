3aebee50785ffe50454284ddc745bf38cbf413c0
Mohamed Elhwary <mh.hwary@yahoo.com>
2021-11-22 02:27:29 +0200

    add tests, fetch_github_url_repos

--- trending_repos/github/tests.py
+++ trending_repos/github/tests.py
@@ -1,7 +1,9 @@
 import datetime
+import json
+from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APITestCase
-import json
+from .utils import fetch_github_url_repos
 
 
 class GithubTests(APITestCase):
@@ -16,3 +18,12 @@ class GithubTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(type(result), dict )
         self.assertEqual(type(result['Python']["Number of repos"]), int )
+
+
+class UtilsTests(TestCase):
+    def test_fetch_github_url_repos(self):
+        github_response = fetch_github_url_repos(days=30, nom_repos=100)
+        self.assertEqual(github_response.status_code, status.HTTP_200_OK)
+
+
+
